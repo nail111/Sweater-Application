484e47d98e7aecffc5b61cdd39c764e3d0d58555
Nail <nailtagiyev1999@gmail.com>
2022-12-12 22:16:26 +0400

    Start project

---
+++ src/test/java/com/sweater/sweaterapp/SweaterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweater.sweaterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweaterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

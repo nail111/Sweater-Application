80fb29830a5b58b6510522e9947f0decfc8890ff
Nail <nailtagiyev1999@gmail.com>
2022-12-12 23:43:31 +0400

    add filter

--- src/main/java/com/sweater/controller/GreetingController.java
+++ src/main/java/com/sweater/controller/GreetingController.java
@@ -1,13 +1,20 @@
 package com.sweater.controller;
 
+import com.sweater.model.Message;
+import com.sweater.repo.MessageRepo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
+@RequiredArgsConstructor
 public class GreetingController {
+
+    private final MessageRepo messageRepo;
 
     @GetMapping("/greeting")
     public String greeting(
@@ -20,7 +27,30 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model) {
-        model.put("some", "Hello, let's code!!!!");
+        model.put("messages", messageRepo.findAll());
+        return "main";
+    }
+
+    @PostMapping
+    public String addMessage(
+            @RequestParam(required = false) String text,
+            @RequestParam(required = false) String tag,
+            @RequestParam(name = "filter", required = false, defaultValue = "") String filter,
+            Map<String, Object> model
+    ) {
+
+        if ((text != null && !text.isBlank() && !text.isEmpty()) || (tag != null && !tag.isEmpty() && !tag.isBlank())) {
+            Message message = new Message(text, tag);
+            messageRepo.save(message);
+            model.put("messages", messageRepo.findAll());
+        }
+
+        if (filter != null && !filter.isEmpty() && !filter.isBlank()) {
+            model.put("messages", messageRepo.findByTag(filter));
+        } else {
+            model.put("messages", messageRepo.findAll());
+        }
+
         return "main";
     }
 }

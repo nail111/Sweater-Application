a5ec2b7c6c3b31790e3c1e02d60816abfd179078
Nail <nailtagiyev1999@gmail.com>
2022-12-15 15:06:42 +0400

    add security

---
+++ src/main/java/com/sweater/controller/RegistrationController.java
@@ -0,0 +1,40 @@
+package com.sweater.controller;
+
+import com.sweater.model.Role;
+import com.sweater.model.User;
+import com.sweater.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+public class RegistrationController {
+
+    private final UserRepo userRepo;
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            model.put("message", "User is already exists");
+            return "registration";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+
+        return "redirect:/login";
+    }
+}

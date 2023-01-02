78de886f44b41f81dc383b4035ccbacfeb2eae49
Nail <nailtagiyev1999@gmail.com>
2023-01-02 13:32:06 +0400

    add pagination

--- src/main/java/com/sweater/controller/MainController.java
+++ src/main/java/com/sweater/controller/MainController.java
@@ -6,6 +6,10 @@ import com.sweater.repo.MessageRepo;
 import com.sweater.utils.ControllerUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -39,16 +43,21 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
-        Iterable<Message> messages = messageRepo.findAll();
+    public String main(
+            @RequestParam(required = false, defaultValue = "") String filter,
+            Model model,
+            @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        Page<Message> page;
 
         if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
+            page = messageRepo.findByTag(filter, pageable);
         } else {
-            messages = messageRepo.findAll();
+            page = messageRepo.findAll(pageable);
         }
 
-        model.addAttribute("messages", messages);
+        model.addAttribute("page", page);
+        model.addAttribute("url", "/main");
         model.addAttribute("filter", filter);
 
         return "main";
--- src/main/java/com/sweater/repo/MessageRepo.java
+++ src/main/java/com/sweater/repo/MessageRepo.java
@@ -1,10 +1,12 @@
 package com.sweater.repo;
 
 import com.sweater.model.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface MessageRepo extends JpaRepository<Message, Long> {
-    List<Message> findByTag(String tag);
+    Page<Message> findByTag(String tag, Pageable pageable);
+
+    Page<Message> findAll(Pageable pageable);
 }
